2360db14e8d661db346f1691a7cec2364c67a5e4
Tchowds <zcabtac@ucl.ac.uk-use-Github-Anonymous>
2025-04-26 19:27:39 +0100

    comment torchsharp scripts to prevent errors

--- Assets/Scripts/RuntimeML/ModelTraining.cs
+++ Assets/Scripts/RuntimeML/ModelTraining.cs
@@ -1,51 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using UnityEngine;
-using TMPro;
+// Uncomment if TorchSharp is imported
 
-public class ModelTraining : MonoBehaviour
-{
-    // Menu pages to disable and enable after training
-    [SerializeField] private GameObject toDisable;
-    [SerializeField] private GameObject toEnable;
-    // Text to say what epoch of training we are on
-    [SerializeField] private TMP_Text trainingText;
+// using System.Collections;
+// using System.Collections.Generic;
+// using System.Threading.Tasks;
+// using UnityEngine;
+// using TMPro;
 
-    private bool trainingCompleted = false;
+// public class ModelTraining : MonoBehaviour
+// {
+//     // Menu pages to disable and enable after training
+//     [SerializeField] private GameObject toDisable;
+//     [SerializeField] private GameObject toEnable;
+//     // Text to say what epoch of training we are on
+//     [SerializeField] private TMP_Text trainingText;
 
-    void OnEnable()
-    {
-        if(DataInterface.moreThanOnePose()){
-            var (trainingInputs, trainingOutputs) = DataInterface.convertToTrainingData();
-            StartCoroutine(TrainCoroutine(trainingInputs, trainingOutputs));
-        } else
-        {
-            toDisable.SetActive(false);
-            toEnable.SetActive(true);
-        }
+//     private bool trainingCompleted = false;
 
-    }
+//     void OnEnable()
+//     {
+//         if(DataInterface.moreThanOnePose()){
+//             var (trainingInputs, trainingOutputs) = DataInterface.convertToTrainingData();
+//             StartCoroutine(TrainCoroutine(trainingInputs, trainingOutputs));
+//         } else
+//         {
+//             toDisable.SetActive(false);
+//             toEnable.SetActive(true);
+//         }
 
-    private IEnumerator TrainCoroutine(float[][] inputsArr, float[] labelsArr)
-    {
-        // Run training in background
-        Task.Run(() =>
-        {
-            TorchSharpPoses.train(inputsArr, labelsArr);
-            trainingCompleted = true;
-        });
+//     }
 
-        // Wait until the training is completed.
-        yield return new WaitUntil(() => trainingCompleted);
+//     private IEnumerator TrainCoroutine(float[][] inputsArr, float[] labelsArr)
+//     {
+//         // Run training in background
+//         Task.Run(() =>
+//         {
+//             TorchSharpPoses.train(inputsArr, labelsArr);
+//             trainingCompleted = true;
+//         });
 
-        // Training is completed, update the UI
-        toDisable.SetActive(false);
-        toEnable.SetActive(true);
-    }
+//         // Wait until the training is completed.
+//         yield return new WaitUntil(() => trainingCompleted);
 
-    void update()
-    {
-        trainingText.text = "Training Models. Loading..." + TorchSharpPoses.currentEpoch.ToString();
-    }
-}
+//         // Training is completed, update the UI
+//         toDisable.SetActive(false);
+//         toEnable.SetActive(true);
+//     }
+
+//     void update()
+//     {
+//         trainingText.text = "Training Models. Loading..." + TorchSharpPoses.currentEpoch.ToString();
+//     }
+// }
--- Assets/Scripts/RuntimeML/TorchSharpPoses.cs
+++ Assets/Scripts/RuntimeML/TorchSharpPoses.cs
@@ -1,97 +1,99 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// Uncomment if TorchSarp is imported
 
-using TorchSharp;
-using static TorchSharp.torch;
-using static TorchSharp.torch.nn;
-using static TorchSharp.torch.nn.functional;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-// Pose model architecture using TorchSharp
-public class Net : Module<Tensor, Tensor>
-{
-    private Module<Tensor, Tensor> features;
+// using TorchSharp;
+// using static TorchSharp.torch;
+// using static TorchSharp.torch.nn;
+// using static TorchSharp.torch.nn.functional;
 
-    public Net(string name, int numClasses, Device device = null) : base(name)
-    {
-        features = Sequential(
-            ("l1", Linear(22,32)),
-            ("r1", ReLU(inplace: true)),
-            ("l2", Linear(32,32)),
-            ("r2", ReLU(inplace: true)),
-            ("l3", Linear(32,numClasses))
-        );
+// // Pose model architecture using TorchSharp
+// public class Net : Module<Tensor, Tensor>
+// {
+//     private Module<Tensor, Tensor> features;
 
-        RegisterComponents();
+//     public Net(string name, int numClasses, Device device = null) : base(name)
+//     {
+//         features = Sequential(
+//             ("l1", Linear(22,32)),
+//             ("r1", ReLU(inplace: true)),
+//             ("l2", Linear(32,32)),
+//             ("r2", ReLU(inplace: true)),
+//             ("l3", Linear(32,numClasses))
+//         );
 
-        this.to(TorchSharp.DeviceType.CPU);
-    }
+//         RegisterComponents();
 
-    public override Tensor forward(Tensor input)
-    {
-        return features.forward(input);
-    }
-}
+//         this.to(TorchSharp.DeviceType.CPU);
+//     }
 
-// Wrapper for TorchSharp model training and prediction
-public static class TorchSharpPoses
-{
-    private static Net net;
-    public static int currentEpoch = 0;
+//     public override Tensor forward(Tensor input)
+//     {
+//         return features.forward(input);
+//     }
+// }
+
+// // Wrapper for TorchSharp model training and prediction
+// public static class TorchSharpPoses
+// {
+//     private static Net net;
+//     public static int currentEpoch = 0;
     
-    // Train the model using TorchSharp
-    public static void train(float[][] inputsArr, float[] labelsArr)
-    {
-        try
-        {
-            // Calculates the number of classes based on the labels
-            int numClasses = 1;
-            for (int i = 0; i < labelsArr.Length; i++)
-            {
-                if (labelsArr[i] > numClasses) numClasses = (int)labelsArr[i];
-            }
-            numClasses++;
+//     // Train the model using TorchSharp
+//     public static void train(float[][] inputsArr, float[] labelsArr)
+//     {
+//         try
+//         {
+//             // Calculates the number of classes based on the labels
+//             int numClasses = 1;
+//             for (int i = 0; i < labelsArr.Length; i++)
+//             {
+//                 if (labelsArr[i] > numClasses) numClasses = (int)labelsArr[i];
+//             }
+//             numClasses++;
 
-            net = new Net("net", numClasses);
-            var optimizer = torch.optim.SGD(net.parameters(), 0.01f);
-            var loss = torch.nn.CrossEntropyLoss();
+//             net = new Net("net", numClasses);
+//             var optimizer = torch.optim.SGD(net.parameters(), 0.01f);
+//             var loss = torch.nn.CrossEntropyLoss();
 
-            // Convert the inputs and labels to TorchSharp tensors
-            var inputsMat = new float[inputsArr.Length, inputsArr[0].Length];
-            for (int i = 0; i < inputsArr.Length; i++)
-            {
-                for (int j = 0; j < inputsArr[0].Length; j++)
-                {
-                    inputsMat[i, j] = inputsArr[i][j];
-                }
-            }
+//             // Convert the inputs and labels to TorchSharp tensors
+//             var inputsMat = new float[inputsArr.Length, inputsArr[0].Length];
+//             for (int i = 0; i < inputsArr.Length; i++)
+//             {
+//                 for (int j = 0; j < inputsArr[0].Length; j++)
+//                 {
+//                     inputsMat[i, j] = inputsArr[i][j];
+//                 }
+//             }
 
-            var inputs = torch.tensor(inputsMat, dtype: ScalarType.Float32);
-            var labels = torch.tensor(labelsArr).to_type(ScalarType.Int64);
+//             var inputs = torch.tensor(inputsMat, dtype: ScalarType.Float32);
+//             var labels = torch.tensor(labelsArr).to_type(ScalarType.Int64);
 
-            // Training loop
-            for (int epoch = 0; epoch < 10; epoch++)
-            {
-                currentEpoch = epoch;
-                optimizer.zero_grad();
-                var lossValue = loss.forward(net.forward(inputs), labels);
-                lossValue.backward();
-                optimizer.step();
-            }
-        }
-        catch (System.Exception ex)
-        {
-            Debug.LogError("error: " + ex);
-        }
-    }
+//             // Training loop
+//             for (int epoch = 0; epoch < 10; epoch++)
+//             {
+//                 currentEpoch = epoch;
+//                 optimizer.zero_grad();
+//                 var lossValue = loss.forward(net.forward(inputs), labels);
+//                 lossValue.backward();
+//                 optimizer.step();
+//             }
+//         }
+//         catch (System.Exception ex)
+//         {
+//             Debug.LogError("error: " + ex);
+//         }
+//     }
 
-    // Inference method to predict the class of a given input
-    public static float[] predict(float[] inputsArr)
-    {
-        var inputs = torch.tensor(inputsArr);
-        var outputs = net.forward(inputs);
-        var probabilities = torch.nn.functional.softmax(outputs, 0);
-        return probabilities.data<float>().ToArray();
-    }
+//     // Inference method to predict the class of a given input
+//     public static float[] predict(float[] inputsArr)
+//     {
+//         var inputs = torch.tensor(inputsArr);
+//         var outputs = net.forward(inputs);
+//         var probabilities = torch.nn.functional.softmax(outputs, 0);
+//         return probabilities.data<float>().ToArray();
+//     }
 
-}
+// }
